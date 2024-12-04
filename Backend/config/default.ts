a59be813a2851ca52c8c1aaacce96fbb6a82a13e
Akash sharma <akashsharma90099@gmail.com>
2024-12-05 01:12:50 +0530

    fix: update database configuration to use environment variables

--- Backend/config/default.ts
+++ Backend/config/default.ts
@@ -1,27 +1,16 @@
-// export default {
-//   port: 4000,
-//   basePath: "",
-//   UI_URL: "http://localhost:5173",
-//   JWTExpiryTime: "30m",
-//   dbConfig: {
-//     database: "rewards_management",
-//     userName: "rewards_management_user",
-//     password: "ixWKTzWK1auMJzYpe1ZtXYSaW1LZlwwY",
-//     host: "dpg-ck473rvqj8ts73b6dvi0-a.singapore-postgres.render.com",
-//     port: 5432,
-//   },
-// };
+import dotenv from 'dotenv';
 
+dotenv.config();
 export default {
   port: 4000,
   basePath: "",
   UI_URL: "http://localhost:5173",
   JWTExpiryTime: "30m",
   dbConfig: {
-    database: "admindatabase",
-    userName: "postgres",
-    password: "admin",
-    host: "localhost",
-    port: 5432,
+    database: process.env.DB_NAME,
+    userName: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432'),
   },
 };
